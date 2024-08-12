360427af362f752088eab70e75c61028d6b377ba
bara1121 <como7723@gmail.com>
2024-08-12 21:28:00 +0800

    update the code

---
+++ element_practice/element.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    const main = document.getElementById('main')
+    const bigText = document.createElement('h1')
+    bigText.innerText = 'Hi 阿孟'
+    main.append(bigText)
+    console.log(main)
+}
